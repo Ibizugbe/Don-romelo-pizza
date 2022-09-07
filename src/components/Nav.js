12e45bc522c8bac9b46bbcb410186fc839dbc5d1
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-09-07 12:46:32 +0100

    install reactt router dom

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import "./nav.css";
 
 function Nav() {
   return (
     <>
       <header>
+        <nav>
+          <ul></ul>
+        </nav>
         <nav>
           <ul>
             <li>Pizzas</li>
